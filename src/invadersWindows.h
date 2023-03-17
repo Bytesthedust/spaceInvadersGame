2b5b887f1c1ccd09a275a7d063fd8a9a8da2643f
Bytesthedust <47130778+Bytesthedust@users.noreply.github.com>
2023-03-16 20:18:00 -0700

    Update invadersWindows.h
    included additional function definitions to initialize ncurses and windows

--- src/invadersWindows.h
+++ src/invadersWindows.h
@@ -1,7 +1,18 @@
-//header file for invaderWindows
-
+//header file for invadersWindows
+//
 #pragma once
+#include "invaders.hpp"
 
-void initialize_curses();
+#ifndef __spaceInvaders__invadersWindows__
+#define __spaceInvaders__invadersWindows__
+
+void initialize_windows();
+
+void show_window(enum window_id);
+
+void initialize_curses(void);
 
 void create_window(WINDOW ** window, int height, int width, int y, int x);
+
+void create_menu(WINDOW ** menuwin, int height, int width, int y, int x);
+#endif /* defined(__spaceInvaders_invadersWindows__) */
