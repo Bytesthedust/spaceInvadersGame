98f1771fddc09421e8c16b500dd6301551c3f238
Bytesthedust <47130778+Bytesthedust@users.noreply.github.com>
2023-03-20 16:13:57 -0700

    Create invadersScore.h
    header file with functions for score mechanics

---
+++ src/invadersScore.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <ncurses.h>
+
+class Scoreboard
+{
+	WINDOW *score_win;
+
+	public:
+		Scoreboard()
+		{
+		}
+
+		Scoreboard(int width, int y, int x)
+		{
+			score_win = newwin(1, width, y, x);
+		}
+
+		void initialize(int initial_score)
+		{
+			clear();
+			mvwprintw(score_win,0,0,"Score:");
+			updateScore(initial_score);
+			refresh();
+		}
+
+		void updateScore(int score)
+		{
+			mvwprintw(score_win, 0, score_win->_maxx-10, "%11llu", score); //allows a space for 11 characters
+
+		}
+
+
+		void clear()
+		{
+			wclear(score_win);
+
+		}
+
+		void refresh()
+		{
+			wrefresh(score_win);
+		}
+
+};
