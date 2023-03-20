3fa9ef4aca701cd58029de533d5869b97f8e1ce0
Bytesthedust <47130778+Bytesthedust@users.noreply.github.com>
2023-03-20 16:18:42 -0700

    Update spaceInvaders.cpp
    updated main file including main game loop

--- src/spaceInvaders.cpp
+++ src/spaceInvaders.cpp
@@ -1,33 +1,40 @@
 #include <ostream>
 #include <iostream>
 #include <ncurses.h>
-
 #include "invadersWindows.hpp"
 #include "invaders.hpp"
+#include "invadersGame.hpp"
+#include "drawable.hpp"
+#include "invadersBoard.hpp"
+
+
 
 using namespace std;
 
 
 int main(int argc, char** argv)
 {
+	//ncurses start
 	
+	//NOTE: x coordinates increase to the right, y coordinates increase fom top to bottom
 	
-	//start ncurses
-	initialize_curses();
+	initialize_curses(); //start ncurses
 	
-	//create windows
-	initialize_windows();
-	
-	//show main menu
-	show_window(WIN_MENU);
+	invadersGame game(WIN_GAME_HEIGHT, WIN_GAME_WIDTH, 180);
 
-	//get screen size
-	int yMax, xMax;
-	getmaxyx(stdscr, yMax, xMax);
-	
-	
+	while(1)
+	{
+		game.processInput();
+		game.updateState();
+		game.redraw();
+	}
+
+	//initialize_windows();
+
+	//show_window(WIN_MENU);
+
+
 	//program waits before exiting
-	getch();
 	endwin();
 
 	return 0;
