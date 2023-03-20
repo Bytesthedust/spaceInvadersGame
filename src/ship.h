b0449e3390143a12cd2cf5c6be74f19515bb0f7e
Bytesthedust <47130778+Bytesthedust@users.noreply.github.com>
2023-03-20 16:15:21 -0700

    Update ship.h

--- src/ship.h
+++ src/ship.h
@@ -3,21 +3,74 @@
 #pragma once
 #include <ncurses.h>
 #include "drawable.hpp"
+#include "invaders.hpp"
+//#include "bolt.hpp"
 
 //directions that the ship can move in
 enum Direction
 {
-	left = 2;
-	right = 2;
+	left,
+	right
 };
 
-class Ship: public Drawable
+class Sprite: public Drawable
 {
 	public:
-		Ship(int y, int x)
+		Sprite()
 		{
-			this->y = y;
-			this->x = x;
+			this->x = this->y = 0;
 			this->icon = '^';
+		}
+
+		Sprite(int y, int x)
+		{
+			this->x = x;
+			this->y = y;
+			this->icon = '^';
+		}
+
+};
+
+
+class Ship: Sprite
+{
+	Sprite shipSprite;
+	Direction cur_direction;
+	public:
+		Ship()
+		{
+			cur_direction = right;
+		}
+
+		Direction getDirection()
+		{
+			return cur_direction;
+		}
+
+		void setDirection(Direction d)
+		{
+			cur_direction = d;
+		}
+
+		//shootBolt method: spawn in front of ship. While bolt is in empty space, continue forward. If bolt hits non-empty space, destroy object and bolt
+
+		Sprite move()
+		{
+			int row = shipSprite.getY();
+			int col = shipSprite.getX();
+
+			switch(cur_direction)
+			{
+				case left:
+					col--;
+					break;
+				case right:
+					row--;
+					break;
+				default:
+					break;
+			}
+
+			return Sprite(row, col);
 		}
 };
