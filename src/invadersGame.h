006650ab010a2705d9880136faee66f9678729b2
Bytesthedust <47130778+Bytesthedust@users.noreply.github.com>
2023-05-02 01:11:50 -0400

    Update invadersGame.h

--- src/invadersGame.h
+++ src/invadersGame.h
@@ -16,6 +16,7 @@
 #include "lives.hpp"
 #include "bolt.hpp"
 #include "shield.hpp"
+
 class invadersGame
 {
 	public:
@@ -62,58 +63,49 @@ class invadersGame
 			//fourth shields
 			createShields(52);
 			
-			displayShields(&shield);
+			displayShields(&shield); //add shields to board
 
 			//create aliens
 			createAlien(3, 6, 'A'); //first row
-			//createAlien(5, 6, 'O'); //second row
-			//createAlien(7, 6, 'G'); //third row
-			//createAlien(9, 6, 'X'); //fourth row
+			createAlien(5, 6, 'O'); //second row
+			createAlien(7, 6, 'G'); //third row
+			createAlien(9, 6, 'X'); //fourth row
 
-			displayAliens(alien);
-			moveAlien(alien); //begin alien movement
+			displayAliens(alien); //add aliens to board
 
 		}
 
 
-		void processInput()
+		chtype processInput()
 		{
 			chtype input = board.getInput();
 
-			switch(input)
-			{
-				//player presses left arrow key to move left
-				case KEY_LEFT:
-					moveShip(left);
-					break;
-
-				//player presses right arrow key to move right
-				case KEY_RIGHT:
-					moveShip(right);
-					break;
-
-				//player presses spacebar to shoot bolt
-
-				case ' ':
-					createBolt(ship->getY()-1, ship->getX()); //initial position of the bolt in front of the ship
-					moveBolt();
-					break;
-
-				//player presses p key to pause/unpause game
-				case 'p':
-					board.setTimeout(-1);
-					while(board.getInput() != 'p');
-					board.setTimeout(1000);
-					break;
-
-			}
+			return input;
 		}
 
 
-		void updateState()
+		void updateState(unsigned tick)
 		{
 			//logic for animation->update positions for sprites at the same time + usleep in main loop
 			//60 fps => 1000/fps = 16.6 fpms
+
+			chtype input = processInput();
+
+			//update ship position
+			if(input == KEY_LEFT || input == KEY_RIGHT)
+				updateShip(input);
+
+			//fire bolt if spacevar is pressed
+			if(input == ' ')
+				createBolt(ship->getY()-1, ship->getX());
+			updateBolt();
+			
+			//alien movement
+			if(tick % 1 == 0)
+			{
+				updateAlien(alien);
+				alien = alien->next;
+			}
  
 		}
 
@@ -154,7 +146,13 @@ class invadersGame
 	private:
 		Board board;
 		bool game_over;
+	
 		
+		
+		int shift = 1; //alien x shift
+
+		int yshift = -1;
+
 		Ship* ship = NULL; //class for ship 
 		Empty* space; //class for empty space
 		Bolt* bolt = NULL; //class for bullet
@@ -185,19 +183,21 @@ class invadersGame
 
 		}
 
-		//create ship icon	
+		//create ship sprite
 		void createShip(int y, int x)
 		{
 			ship = new Ship(y, x);
 			board.add(*ship);
 		}
 
+		//create bolt sprite
 		void createBolt(int y, int x)
 		{
 			bolt = new Bolt(y, x);
 			board.add(*bolt);
 		}
 		
+		//create alien bomb sprite
 		void createAlienBolt(int y, int x)
 		{
 			bomb = new Bolt(y,x);
@@ -231,291 +231,143 @@ class invadersGame
 			board.refresh();
 		}
 
+		//show aliens on board
 		void displayAliens(Alien* head)
 		{
 			Alien* temp = head;
 			do
 			{
 				board.add(*temp);
-				board.refresh();
 				temp = temp->next;
 			} while(temp != head);
 		}
 
-	
 
-		//SPRITE DESTRUCTION
-		//when collision occurs, destroy bolt and sprite
-		void destroySprite(int y, int x, int next_y)
+		//update functions
+
+		//update ship position
+		void updateShip(chtype ch)
 		{
-			board.addAt(y, x, ' '); //destroy bolt
-			board.addAt(next_y, x, ' '); //destroy sprite
-		}
-
-		//ship takes damage 
-		void shipHit()
-		{
-			board.addAt(ship->getY(), ship->getX(), ' ');
-			delete(ship);
-			lives -= 1; //player loses a life
-			livesBoard.updateLives(lives);
-			createShip(SHIP_Y, SHIP_X); //respawn ship at default location
-		}
-		
-
-		//SPRITE MOVEMENT
-		//function to change position of ship icon
-		void moveShip(Direction d)
-		{
-
-			//get current position of ship
 			int ship_col = ship->getX();
 			int ship_row = ship->getY();
 
 			board.addAt(ship_row, ship_col, ' ');
 
-			//BUG: ship is moving beyond game window(Resolved by adding a conditional to ship movement)
-
-			//check if ship is moving beyond the window and update ship position
-
-			switch(d)
+			switch(ch)
 			{
-				case left:
-					if(ship_col>1) 
+				case KEY_LEFT:
+					if(ship_col > 1)
+					{
 						ship_col -= 1;
-					break;
+						ship->setX(ship_col);
+					}
 
-				case right:
+					break;
+				case KEY_RIGHT:
 					if(ship_col < 73)
+					{
 						ship_col += 1;
+						ship->setX(ship_col);
+					}
+
 					break;
 				default:
 					break;
 			}
 
-			//BUG: when ship moves, leaves trail of ghost ships.(Resolved by refreshing window)
-
-			delete(ship); //memory cleanup of old ship
-			createShip(ship_row, ship_col);
-			board.refresh();
-		
-		}
-
-		void destroyAlien(Alien* target, Bolt* bullet)
-		{
-			Alien* temp = target;
-			while(temp->getY() != bullet->getY() && temp->getX() != bullet->getX())
-				temp = temp->next;
-
-			temp->setIcon(' ');
-			target->deleteAlien(&temp, ' ');
+			board.add(*ship);
 
 		}
 
-		//BUG: when bolt fires, freezes ship in place
-		void moveBolt()
+		//update bolt position
+		void updateBolt()
 		{
-			//get the coordinates of the bolt
+			if(bolt == NULL)
+				return;
+
 			int bolt_col = bolt->getX();
+			int bolt_row = bolt->getY();
 
-			int next_y = bolt->getY();
-
-			//animating bolt movement
-
-			for(next_y; next_y > 0; --next_y)
-			{
-				chtype ahead = board.getCharAt(next_y, bolt_col); //detect incoming sprite
-				chtype input = board.getInput();
-
-				//allows movement during bolt travel
-				if(input == KEY_LEFT)
-					moveShip(left);
-				if(input == KEY_RIGHT)
-					moveShip(right);
-
-				//collision detection
-
-				//if bolt hits shield
-				if(ahead == '#')
-				{
-					destroySprite(bolt->getY(), bolt->getX(), next_y);
-					delete(bolt);
-					break;
-				}
-
-				//if bolt hits alien
-				else if(ahead == 'A' || ahead == 'O' || ahead == 'G' || ahead=='X')
-				{
-					destroySprite(bolt->getY(), bolt->getX(), next_y);
-					score += alien->getValue(ahead);
-					scoreboard.updateScore(score);
-					destroyAlien(alien, bolt);
-					delete(bolt);
-					break;
-				}
-				
-
-				else
-				{
-					//else bolt continues to the border
-					board.addAt(bolt->getY(), bolt->getX(), ' '); //create an empty space in the bolt old position
-					delete(bolt); //memory management of bolt
-					createBolt(next_y, bolt_col); //create a new bolt given the updated coordinates
-				}
-				
-				board.refresh(); //refresh window to show bolt
-				usleep(80000); //pause execution for 80000 miliseconds or 0.08 seconds. This simulates "flipping" to the next frame in the animation
-
-			}
-
-			if(next_y <= 0) //if bolt reaches border
-			{
-				board.addAt(bolt->getY(), bolt->getX(), ' ');
-				delete(bolt);
-			}
-
-
-		}
-		
-		
-		//function for enemy movement
-		void moveAlien(Alien* head)
-		{
 			
-			Alien* temp = head;
+			if(bolt_row == 1)
+			{
+				board.addAt(bolt_row, bolt_col, ' ');
+				bolt = NULL;
+				return;
+			}
 
+			//collision detection
 
-			int direction = 0; //flag to determine alien direction
-			int shift = 0; //flag to determine if aliens need to be moved down
-			int shoot; //flag for shooting
-
-			//srand(time(0)); //seed for shoot variable random value
 			
-			//begin movement
-
-			//1: traverse through aliens and update positions
-			//2: if temp = head, refresh the board and continue loop
-			//shoot condition: an alien can fire a bomb only if there is no alien underneath it
-
-			while(true)
+			//bolt hits alien
+			if(alien->getY() == bolt->getY() && alien->getX() == bolt->getX())
 			{
-				processInput(); //allow player movement
-
-				board.addAt(temp->getY(), temp->getX(), ' ');
-
-				//implement player movement
-
-				if(temp == head)
-					shift = 0;
-					board.refresh();
-
-				if(direction == 0 && shift == 0) //move right
-				{
-					temp->alienLeft();
-				}
-
-				if(direction == 1 && shift == 0) // move left
-				{
-					temp->alienRight();
-				}
-
-				if(temp->getX() < 2)
-				{
-					direction = 0;
-					shift = 1;
-				}
-
-				if(temp->getX() > 72)
-				{
-					direction = 1;
-
-					shift = 1;
-				}
-
-				if(shift == 1)
-				{
-					temp->alienDown();
-				}
-
-
-				board.add(*temp);
-				temp = temp->next;
-
-				usleep(1000);
-
+				board.addAt(alien->getY(), alien->getX(), ' ' );
+				alien->setIcon(' ');
+				board.addAt(bolt_row, bolt_col, ' ');
+				score += alien->getValue(alien->getIcon());
+				scoreboard.updateScore(score);
 			}
 
 
-
+			else
+			{
+				board.addAt(bolt_row, bolt_col, ' ');
+				bolt_row -= 1;
+				bolt->setY(bolt_row);
+				board.add(*bolt);
+			}
 		}
 
-
-
-
-		
-		void moveAlienBolt()
+		//update alien position
+		void updateAlien(Alien* head)
 		{
-			//get the coordinates of the bolt
-			createAlienBolt(alien->getY()+1, alien->getX());
-
-			int bolt_col = bomb->getX();
-			int next_y = bomb->getY();
 
 
-			//animating bolt movement
-			for(next_y; next_y > 0; ++next_y)
+			board.addAt(head->getY(), head->getX(), ' ');
+
+			int next_x = head->getX() + shift;
+			//int next_y = head->getY() + 1; //NOTE: Find way to shift every alien down once border is reached
+
+			if(next_x >= 35 || next_x <= 1)
 			{
-				chtype ahead = board.getCharAt(next_y, bolt_col); //detect incoming sprite
-				chtype input = board.getInput();
-
-				
-				//allows movement during bolt travel
-				if(input == KEY_LEFT)
-					moveShip(left);
-				if(input == KEY_RIGHT)
-					moveShip(right);
-
-				//processInput();
-
-
-				//collision detection
-
-				//if bolt hits shield
-				if(ahead == '#')
-				{
-					destroySprite(bomb->getY(), bomb->getX(), next_y);
-					delete(bomb);
-					break;
-				}
-
-				//if bolt hits player
-				else if(ahead == '^')//NOTE: Crash occurs
-				{
-					board.addAt(bomb->getY(), bomb->getX(), ' ');
-					delete(bomb);
-					shipHit();
-					break;
-				}
-
-				else
-				{
-
-					//else bolt continues to the border
-					board.addAt(bomb->getY(), bomb->getX(), ' '); //create an empty space in the bolt old position
-					delete(bomb); //memory management of bolt	
-					createAlienBolt(next_y, bolt_col); //create a new bolt given the updated coordinates
-				
-				}
-				board.refresh(); //refresh window to show bolt
-				usleep(80000); //pause execution for 80000 miliseconds or 0.08 seconds. This simulates "flipping" to the next frame in the animation
-
+				shift *= -1;
+				int next_y = head->getY() + 1;
+				if(head->getY() < next_y)
+					head->setY(next_y);
 			}
-			if(bomb->getY() > 28)
+
+			else
 			{
-				board.addAt(bomb->getY(), bomb->getX(), ' ');
+
+				head->setX(next_x);
+				//head->setY(next_y);
 			}
+
+
+
+			board.add(*head);
 
 
 		}
+
+		//update bomb position
+		void updateBomb()
+		{
+			int bomb_col = bomb->getX();
+			int bomb_row = bolt->getY();
+
+			if(bomb_row >= 30)
+				return;
+
+			else
+			{
+				bomb_row += 1;
+				bomb->setY(bomb_row);
+				board.addAt(--bomb_row, bomb_col, ' ');
+			}
+
+		}	
+		
 
 };
